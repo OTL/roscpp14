c99b51a72d0e6aa7dccc758b933500ee715e4cb2
Takashi Ogura <t.ogura@gmail.com>
2015-08-28 01:19:03 +0900

    Support Ubuntu14.04

---
+++ cmake/roscpp14.cmake
@@ -0,0 +1,8 @@
+if (UNIX)
+  # for Ubuntu14.04
+  # sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  # sudo apt-get update
+  # sudo apt-get install g++-4.9
+  set(CMAKE_CXX_COMPILER g++-4.9)
+endif (UNIX)
+add_definitions(-std=c++14)
