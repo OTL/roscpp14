41d1b25784c952119c0fdaf6b867cc1f3197cb78
Takashi <t.ogura@gmail.com>
2015-08-28 22:20:07 +0900

    Fix for OSX

--- cmake/roscpp14.cmake
+++ cmake/roscpp14.cmake
@@ -1,8 +1,9 @@
-if (UNIX)
+if (APPLE)
+else (APPLE)
   # for Ubuntu14.04
   # sudo add-apt-repository ppa:ubuntu-toolchain-r/test
   # sudo apt-get update
   # sudo apt-get install g++-4.9
   set(CMAKE_CXX_COMPILER g++-4.9)
-endif (UNIX)
+endif (APPLE)
 add_definitions(-std=c++14)
